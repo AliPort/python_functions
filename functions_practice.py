373696d7d1afd72f6b152ebe5139f8578db21282
AliPort <101525919+AliPort@users.noreply.github.com>
2022-11-12 10:55:02 -0600

    fixed functions to be able to run

--- functions_practice.py
+++ functions_practice.py
@@ -4,7 +4,7 @@ def hello():
 def pack(Blue,Pink,Purple):
     return[Blue,Pink,Purple]
 
-def eat_lunch(my_list):
+def  eat_lunch(my_list):
     if len(my_list) == 0:
         print("My lunchbox is empty!")
     else:
@@ -13,3 +13,127 @@ def eat_lunch(my_list):
                 print(f"First I eat{my_list[0]}")
             else:
                 print(f"Next I eat {my_list[i]}")
+
+# arb_args — Takes in any number of arguments and prints them one at a time. 
+
+def arb_args(*args):
+  for a in args:
+    print(a)
+
+# inner_func — Takes in two integers. Two nested functions should perform separate, distinct math operations using the integers. The result of both nested functions should then be added together and printed.
+
+def inner_func(x,y):
+  def inner_1():
+    return x+y
+  def inner_2():
+    return x-y
+  print(inner_1()+inner_2())
+
+# lunch_lady — Takes in two strings: a student's name and their lunch preference. The function should print both strings. If a lunch preference is not given, "Mystery Meat" should be printed instead.
+
+def lunch_lady(name, lunch="Mystery Meat"):
+  print(name, lunch)
+
+# sum_n_product — Accepts two integers and returns both the sum and the product.
+def sum_n_product(x,y):
+  return x+y,x*y
+
+# alias_arb_args — Should be arb_args but assigned an alias. Remember, variables can hold functions in Python just like they can in JS. Alternatively, you can call a function from inside another function.
+alias_arb_args = arb_args
+
+# arb_mean — Accepts any number of integers and prints their average.
+def arb_mean(*args):
+  total = 0
+  for a in args:
+    total += a
+  print(a/len(args))
+
+# arb_longest_string — Accepts any number of strings and returns the longest one.
+def arb_longest_string(*args):
+  long = 0
+  longest = ""
+  for a in args:
+    if len(a) > long:
+      long = len(a)
+      longest = a
+  return longest
+  
+
+# 1. name_args — Accepts any number of named arguments and prints them in the pattern key : value one at a time.
+
+def name_args(**kwargs):
+  for k in kwargs.keys():
+    print(f"{k}:{kwargs[k]}")
+
+name_args(name="Randon", weather="sunny",location="park",time=3)
+# 2. all_true — Returns True if all the elements in an iterable are true. Hint: there is an existing built-in function that could be very helpful here.
+
+def all_true(iter):
+  return all(iter)
+
+print(all_true([True,True,True]))
+print(all_true((True, False)))
+
+# 3. one_true — Returns True if one of the elements in an iterable is true. Hint: there is an existing built-in function that could be very helpful here.
+
+def one_true(iter):
+  return any(iter)
+
+print(one_true([True,True,True]))
+print(one_true([False, False, False]))
+print(one_true((True, False)))
+
+# 4. recursive_factorial — Uses recursion to find the factorial of an integer.
+
+def recursive_factorial(n):
+  if n <= 1:
+    return 1
+  else:
+    return n * recursive_factorial(n-1)
+
+print(recursive_factorial(3))
+print(recursive_factorial(6))
+
+# 5. recursive_deduplicate — Recursively removes all adjacent duplicate letters from a string.
+
+def recursive_deduplicate(my_str,i=0):
+  # if our string is empty or only has 1 thing, it's got no duplicates
+  # if i is at the end of the string, no duplicates are left
+  if len(my_str) <= 1 or i == len(my_str)-1:
+    return my_str
+  else:
+    # str at current position is same as next position
+    if my_str[i] == my_str[i+1]:
+      return recursive_deduplicate(my_str[0:i]+my_str[i+1:],i)
+    else:
+      #no duplicate at current position, check next
+      return recursive_deduplicate(my_str,i+1)
+      
+print(recursive_deduplicate("aaaa"))
+print(recursive_deduplicate("aaba"))
+print(recursive_deduplicate("apple"))
+print(recursive_deduplicate(""))
+
+# 6. recursive_reverse — Uses recursion to reverse a string.
+def recursive_reverse(str, i=0):
+  #empty string case
+  if len(str)==0:
+    return ""
+  #base case
+  elif i == len(str)-1:
+    return str[0]
+  else:
+    #recursive case
+    return str[-1-i] + recursive_reverse(str, i+1)
+
+print(recursive_reverse("aaaa"))
+print(recursive_reverse("aaba"))
+print(recursive_reverse("apple"))
+print(recursive_reverse(""))
+  
+
+hello()
+print(pack("Blue", "Pink", "Purple",))
+eat_lunch([])
+eat_lunch ([ "pizza"])
+eat_lunch ([ "chips", "salad", "fruit"])
